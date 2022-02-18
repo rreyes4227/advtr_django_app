034e213c98272f4fa53ad3906d12d44779392061
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-02-18 15:56:52 -0700

    Custom User Model

--- base/models.py
+++ base/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -40,3 +45,5 @@ class Message(models.Model):
             return f'{self.body[0:50]}...'
         else:
             return self.body
+
+
