0c85f908a1fc5ce71e3ac18838a0f0c4b29995a2
Ryan D Reyes <ryan.reyes2012@gmail.com>
2022-02-18 16:11:41 -0700

    avatar added

--- base/models.py
+++ base/models.py
@@ -5,8 +5,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(null=True, unique=True)
+    bio = models.TextField(null=True)
 
+    profile_picture = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
